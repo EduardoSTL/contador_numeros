00cf40b5137caa034333c7411ec5ac807d370998
Eduardo <127428185+EduardoSTL@users.noreply.github.com>
2023-09-13 15:05:09 -0600

    added: comments to the screen class

--- lib/presentacion_screens/counter_screen.dart
+++ lib/presentacion_screens/counter_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
+//clase que extiende de StatelessWidget
 class CounterScreen extends StatelessWidget{
   const CounterScreen({super.key});
 
+  //metodo BUILD que define la interfaz del ususario(layout)
   @override
   Widget build(BuildContext context) {
    return Scaffold(
     //* propiedad del widget Scaffold, que a su vez es un widget por si mismo
-    //Center es el body de nuestra pantalla Scaffold
+    //* Center es el body de nuestra pantalla Scaffold
     //! "propiedad" : "Widget": propiedad del widget nuevo
     //AppBar = similar a un encabezado 
     appBar: AppBar(
@@ -16,7 +18,7 @@ class CounterScreen extends StatelessWidget{
     body: Center(
       //Center tiene solo 1 hijo
       child: Column(
-        //columna tiene varios hijos:
+        //columna tiene varios hijos y los centra y organiza de forma vertical:
         mainAxisAlignment: MainAxisAlignment.center,
         children: const [
           Text('10', style: TextStyle(fontSize: 169, fontWeight: FontWeight.w100),
@@ -25,6 +27,7 @@ class CounterScreen extends StatelessWidget{
         ],
       ),
       ),
+      //boton con la accion "sumar"
     floatingActionButton: FloatingActionButton(
       onPressed: (){
 
